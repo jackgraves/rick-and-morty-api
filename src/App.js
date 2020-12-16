6058125d97479b2bca690dc19e26a89f6e9e4535
Robbie Thandi <robbie.thandi@gmail.com>
2020-12-16 13:09:44 +0000

    Fully working app. Still needs polishing.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      Name: null,
+      Species: null,
+      Gender: null,
+      Location: null,
+      Avatar: null
+    }
+  }
+
+  clearState(){
+    this.setState({
+      Name: null,
+      Species: null,
+      Gender: null,
+      Location: null,
+      Avatar: null
+    })
+  }
+
+  getCharacter() {
+    if (this.state.CharacterNumber) {
+      this.clearState();
+    }
+    // Generates a random number 1-10
+    const randId = Math.floor(Math.random() * 10) + 1
+    fetch('https://rickandmortyapi.com/api/character/' + randId)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          Name: data.name,
+          Species: data.species,
+          Gender: data.species,
+          Location: data.location.name,
+          Avatar: data.image
+        })
+      })
+      .catch(console.log)
+  }
+
+  render () {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <div className="characterInfo">
+            <img src={this.state.Avatar} alt="Avatar of character"/>
+            <p><strong><u>Name:</u></strong> {this.state.Name}</p>
+            <p><strong><u>Species:</u></strong> {this.state.Species}</p>
+            <p><strong><u>Gender:</u></strong> {this.state.Gender}</p>
+            <p><strong><u>Location:</u></strong> {this.state.Location}</p>
+          </div>
+          <button onClick={() => {this.getCharacter()}}>Get Character</button>
+        </header>
+      </div>
+    )
+  };
 }
 
 export default App;
