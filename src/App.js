48ae1c3b02eaaaaa3fe4861b9884dbab84c1b509
Robbie Thandi <robbie.thandi@gmail.com>
2020-12-16 13:52:48 +0000

    fixed gender bug.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {Component} from "react";
 
-class App extends Component{
+class App extends Component {
 
   constructor(props) {
     super(props);
@@ -14,29 +14,16 @@ class App extends Component{
     }
   }
 
-  clearState(){
-    this.setState({
-      Name: null,
-      Species: null,
-      Gender: null,
-      Location: null,
-      Avatar: null
-    })
-  }
-
   getCharacter() {
-    if (this.state.CharacterNumber) {
-      this.clearState();
-    }
-    // Generates a random number 1-10
-    const randId = Math.floor(Math.random() * 10) + 1
-    fetch('https://rickandmortyapi.com/api/character/' + randId)
+    // Generates a random integer 1-10
+    const randomId = Math.floor(Math.random() * 10) + 1
+    fetch('https://rickandmortyapi.com/api/character/' + randomId)
       .then(res => res.json())
       .then((data) => {
         this.setState({
           Name: data.name,
           Species: data.species,
-          Gender: data.species,
+          Gender: data.gender,
           Location: data.location.name,
           Avatar: data.image
         })
@@ -44,12 +31,24 @@ class App extends Component{
       .catch(console.log)
   }
 
+  showImage() {
+    if (this.state.Avatar){
+      return (
+        <img src={this.state.Avatar} alt="Avatar of character"/>
+      )
+    } else {
+      return (
+        <img src="https://via.placeholder.com/300" alt="Placeholder of character"/>
+      )
+    }
+  }
+
   render () {
     return(
       <div className="App">
         <header className="App-header">
           <div className="characterInfo">
-            <img src={this.state.Avatar} alt="Avatar of character"/>
+            {this.showImage()}
             <p><strong><u>Name:</u></strong> {this.state.Name}</p>
             <p><strong><u>Species:</u></strong> {this.state.Species}</p>
             <p><strong><u>Gender:</u></strong> {this.state.Gender}</p>
