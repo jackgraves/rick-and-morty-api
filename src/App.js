4394d93654128715d8862e1b3ab1d8768435eb07
Robbie Thandi <robbie.thandi@gmail.com>
2020-12-19 21:09:01 +0000

    Working functionality for multiple character search. Still needs styling.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,78 @@
 import './App.css';
 import {Component} from "react";
 
+function Character(props) {
+  return (
+    <div className="characterInfo">
+      <img src={props.avatar} alt="Avatar of character"/>
+      <p><strong><u>Name:</u></strong> {props.name}</p>
+      <p><strong><u>Species:</u></strong> {props.species}</p>
+      <p><strong><u>Gender:</u></strong> {props.gender}</p>
+      <p><strong><u>Location:</u></strong> {props.location}</p>
+    </div>
+  )
+}
+
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      Name: null,
-      Species: null,
-      Gender: null,
-      Location: null,
-      Avatar: null
+      name: '',
+      status: 'Alive',
+      results: null
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  getCharacter() {
-    // Generates a random integer 1-10
-    const randomId = Math.floor(Math.random() * 10) + 1
-    fetch('https://rickandmortyapi.com/api/character/' + randomId)
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    // If no name is input it will fetch all characters of the chosen status
+    fetch('https://rickandmortyapi.com/api/character?name=' + this.state.name + '&status=' + this.state.status)
       .then(res => res.json())
       .then((data) => {
         this.setState({
-          Name: data.name,
-          Species: data.species,
-          Gender: data.gender,
-          Location: data.location.name,
-          Avatar: data.image
+          results: data.results,
         })
       })
       .catch(console.log)
-  }
-
-  showImage() {
-    if (this.state.Avatar){
-      return (
-        <img src={this.state.Avatar} alt="Avatar of character"/>
-      )
-    } else {
-      return (
-        <img src="https://via.placeholder.com/300" alt="Placeholder of character"/>
-      )
-    }
+    event.preventDefault();
   }
 
   render () {
+    let characters = []
+    if (this.state.results){
+      this.state.results.forEach(obj => { characters.push(<Character key={obj.id}
+                                                                             name={obj.name}
+                                                                             species={obj.species}
+                                                                             gender={obj.gender}
+                                                                             location={obj.location.name}
+                                                                             avatar={obj.image} />)})
+    }
     return (
       <div className="App">
         <header className="App-header">
-          <div className="characterInfo">
-            {this.showImage()}
-            <p><strong><u>Name:</u></strong> {this.state.Name}</p>
-            <p><strong><u>Species:</u></strong> {this.state.Species}</p>
-            <p><strong><u>Gender:</u></strong> {this.state.Gender}</p>
-            <p><strong><u>Location:</u></strong> {this.state.Location}</p>
-          </div>
-          <button onClick={() => {this.getCharacter()}}>Get Character</button>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Character Name" name="name" value={this.state.name} onChange={this.handleInputChange} />
+            <select name="status" value={this.state.status} onChange={this.handleInputChange}>
+              <option value="Alive">Alive</option>
+              <option value="Dead">Dead</option>
+              <option value="Unknown">Unknown</option>
+            </select>
+            <input type="submit" value="Submit" />
+          </form>
+          {characters}
         </header>
       </div>
     )
